e0bf0a55ca944118089b1989b8b6eb250c859f63
JayCode <vsquare396@gmail.com>
2022-01-09 15:55:33 +0100

    Implement horizontal scrolling cards on the Explore package

--- lib/views/board/saved/tabs/search.dart
+++ lib/views/board/saved/tabs/search.dart
@@ -1,6 +1,7 @@
 import 'package:artisan/core/bus/event_bus.dart';
-import 'package:artisan/core/helper/routes/navigation.dart';
+import 'package:artisan/core/helper/helper_handler.dart';
 import 'package:artisan/core/helper/utils/images.dart';
+import 'package:artisan/core/helper/utils/pallets.dart';
 import 'package:artisan/views/board/widget/card_widget.dart';
 import 'package:artisan/views/widgets/image_loader.dart';
 import 'package:artisan/views/widgets/search_field.dart';
@@ -9,17 +10,28 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SearchTab extends StatelessWidget {
-  const SearchTab({Key? key}) : super(key: key);
+  SearchTab({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    final _colors = [Color(0xff11406C), Color(0xff574C6B), Color(0xffC37F6C)];
+
     return Scaffold(
       body: Padding(
         padding: EdgeInsets.all(16.w),
         child: ListView(
           children: [
-            SearchAndFilter(onFilter: () {
-              eventBus.fire(DrawerEvent(context, true));
-            }),
+            SearchAndFilter(
+                hint: 'Search for gigs, live consultancy, home',
+                onFilter: () {
+                  eventBus.fire(DrawerEvent(context, true));
+                }),
+            SizedBox(height: 16.h),
+            SizedBox(
+              height: 155.h,
+              child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: _colors.map((e) => _skills(context, e)).toList()),
+            ),
             SizedBox(height: 16.h),
             Row(children: [
               ImageLoader(path: AppImages.emoji),
@@ -35,6 +47,56 @@ class SearchTab extends StatelessWidget {
             ...[1, 2, 2, 2, 2, 2, 3].map((_) => CardWidget()).toList()
           ],
         ),
+      ),
+    );
+  }
+
+  SizedBox _skills(BuildContext context, Color e) {
+    return SizedBox(
+      width: 200.w,
+      child: Stack(
+        children: [
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                height: 148,
+                width: Utils.getDeviceWidth(context) * .4,
+                margin: EdgeInsets.only(right: 16.w),
+                decoration: BoxDecoration(
+                  color: e,
+                  borderRadius: BorderRadius.all(Radius.circular(5.r)),
+                ),
+              ),
+              SizedBox(
+                width: Utils.getDeviceWidth(context) * .4,
+                child: Container(
+                  height: 5.h,
+                  width: Utils.getDeviceWidth(context) * .4,
+                  margin: EdgeInsets.only(right: 20.w),
+                  decoration: BoxDecoration(
+                    color: Pallets.grey,
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(8.r),
+                        bottomRight: Radius.circular(8.r)),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            padding: EdgeInsets.only(bottom: 30.h, right: 16.w, left: 16.w),
+            alignment: Alignment.bottomLeft,
+            child: TextView(
+              text: 'Designs, Media & Architecture',
+              maxLines: 2,
+              color: Pallets.white,
+              textOverflow: TextOverflow.ellipsis,
+              fontWeight: FontWeight.w700,
+              textAlign: TextAlign.left,
+            ),
+          ),
+        ],
       ),
     );
   }
