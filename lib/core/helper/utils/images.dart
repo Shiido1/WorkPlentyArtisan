9f6eee2ab098ed311fe02e4633a045b93e0fb7ce
JayCode <vsquare396@gmail.com>
2022-01-10 09:02:37 +0100

    Implement Connected Account screens

--- lib/core/helper/utils/images.dart
+++ lib/core/helper/utils/images.dart
@@ -3,7 +3,7 @@ class AppImages {
 
   static String logo = 'assets/images/launcher_icon.png';
   static String whiteLogo = 'assets/images/launcher_white_icon.png';
-  static String google = 'assets/images/google.png';
+  static String google = 'assets/svgs/google.svg';
   static String meta = 'assets/images/meta.png';
   static String smiling = 'assets/images/smiling.png';
   static String blowWhistle = 'assets/images/blow_whistle.png';
--- lib/views/board/profile/manage/connected_accounts.dart
+++ lib/views/board/profile/manage/connected_accounts.dart
@@ -1,6 +1,11 @@
+import 'package:artisan/core/helper/utils/images.dart';
 import 'package:artisan/core/helper/utils/pallets.dart';
+import 'package:artisan/views/widgets/custom_switch.dart';
 import 'package:artisan/views/widgets/default_appbar.dart';
+import 'package:artisan/views/widgets/text_views.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
 
 class ConnectedAccounts extends StatefulWidget {
   ConnectedAccounts({Key? key}) : super(key: key);
@@ -18,6 +23,46 @@ class _ConnectedAccountsState extends State<ConnectedAccounts> {
           title: 'Connected Accounts',
           centerTitle: true,
           textColor: Pallets.white),
+      body: Padding(
+        padding: EdgeInsets.all(16.w),
+        child: ListView(
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SvgPicture.asset(AppImages.google),
+                SizedBox(width: 10.w),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      TextView(
+                          text: 'Google',
+                          fontWeight: FontWeight.w700,
+                          textOverflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.left),
+                      TextView(
+                          text: 'helloworkplenty@gmail.com',
+                          fontWeight: FontWeight.w400,
+                          textOverflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.left),
+                      SizedBox(height: 8.h),
+                      TextView(
+                          text: 'This account is used to sign in ',
+                          fontWeight: FontWeight.normal,
+                          textOverflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.left),
+                    ],
+                  ),
+                ),
+                SizedBox(width: 10.w),
+                CustomSwitch(onChanged: (value) {}, value: true)
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
